16d155d3b652ced77916251bea9b03ec8c973592
ali1ariel <aamachado@inf.ufsm.br>
2020-05-13 09:50:45 -0300

    restore

--- src/telas/comporLook.js
+++ src/telas/comporLook.js
@@ -1,36 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
-const input = [{
-    _id: "id_do_look_1",
-    name: "nome do look 1",
-    description: "descrição do look 1",
-    clothe_torso: {
-        // Informações da roupa do tronco
-    },
-    clothe_leg: {
-        // Informações da roupa das pernas
-    },
-    clothe_feet: {
-        // Informações do calçado
-    }
-},
-{
-    _id: "id_do_look_2",
-    name: "nome do look 2",
-    description: "descrição do look 2",
-    clothe_torso: {
-        // Informações da roupa do tronco
-    },
-    clothe_leg: {
-        // Informações da roupa das pernas
-    },
-    clothe_feet: {
-        // Informações do calçado
-    }
-}
-]
-
 
 export default class ComporLook extends Component {
     render() {
