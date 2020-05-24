ca1e78dfd03b72b154c1f5ab97343125a9933e38
ali1ariel <aamachado@inf.ufsm.br>
2020-05-23 21:23:56 -0300

    final changes

--- src/telas/guarda-roupas/display-peça.js
+++ src/telas/guarda-roupas/display-peça.js
@@ -1,30 +1,13 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView} from 'react-native'
 
-const input = [{
-    _id: "id_do_look_1",
-    name: "nome do look 1",
-    description: "descrição do look 1",
-    clothe_torso: {
+const clothe = {
         image: "https://i.pinimg.com/originals/f0/28/bd/f028bd62bb1694460545da4b2dd82032.jpg", 
         manufacturer: "Renner",
         clothingModel: "Casaquinho de Lã",
         description: "casaco M, com pedras preciosas"
-    },
-    clothe_leg: {
-        image: "https://cea.vteximg.com.br/arquivos/ids/10941770-468-560/Saia-Jeans-Feminina-Midi-com-Botoes-e-Fenda--Azul-Escuro-9817005-Azul_Escuro_1.jpg?v=637097831575430000", 
-        manufacturer: "C&A",
-        clothingModel: "Saia Jeans",
-        description: "Saia Jeans G, abaixo do Joelho"
-    },
-    clothe_feet: {
-        image: "https://a4.vnda.com.br/ortoponto/2017/08/16/or1031-bota-ortopedica-imobilizadora-de-tornozelo-curta-hidrolight-anatomica-com-antiderrapante-5600.jpg?1502912701", 
-        manufacturer: "UsaFlex",
-        clothingModel: "Bota Ortopédica",
-        description: "Bota Ortopédica tam. 38, em alta na estação, combina com qualquer look"
     }
-}
-]
+
 
 export default class Display extends Component {
     
@@ -37,19 +20,19 @@ export default class Display extends Component {
             <ScrollView showsVerticalScrollIndicator={false}>
                 <View style={styles.imageContainer}>
                     <View style={styles.title}>
-                        <Text style={styles.h1}>{input[0].clothe_leg.clothingModel}</Text>
+                        <Text style={styles.h1}>{clothe.clothingModel}</Text>
                     </View>
           
                     <View  style={styles.boxWithShadow}>
-                        <Image style={styles.logo} source={{uri: input[0].clothe_leg.image}} />
+                        <Image style={styles.logo} source={{uri: clothe.image}} />
                     </View>
           
                     <View style={styles.infomations}>
                         <Text style={styles.h2}>
-                            <Text style={styles.textHighlight}>Fabricante: </Text>{input[0].clothe_leg.manufacturer}
+                            <Text style={styles.textHighlight}>Fabricante: </Text>{clothe.manufacturer}
                         </Text>
                         <Text style={styles.h2}>
-                            <Text style={styles.textHighlight}>Descrição: </Text>{input[0].clothe_leg.description}
+                            <Text style={styles.textHighlight}>Descrição: </Text>{clothe.description}
                         </Text>
                     </View>
           
